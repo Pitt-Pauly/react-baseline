cd2cebc415b82d788c4841003a6ebbf9e8e1c536
Pitt Pauly <pitt.pauly@gmail.com>
2018-08-10 14:07:03 +0100

    adding BrowserRouter for server-side rendering later in the workflow.

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const renderApp = () => {
-  render(<App />, document.getElementById('root'));
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
 };
 renderApp();
 
